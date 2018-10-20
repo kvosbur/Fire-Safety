b64e6290f59ba8e40fb01b81ed0b0d914e36b3db
Kevin Vosburgh <kvosbur@purdue.edu>
2018-10-20 16:12:21 -0400

    Added database call to add problem to database

--- src/main/java/DatabaseAPI.java
+++ src/main/java/DatabaseAPI.java
@@ -53,11 +53,39 @@ public class DatabaseAPI {
         return prob;
     }
 
+    public void addProblem(Problem input) throws SQLException{
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO problem (response, problem_type) VALUES('TEMP',?)");
+        ps.setInt(1, input.type);
+        ps.execute();
+
+        ps = connection.prepareStatement("SELECT problem_id from problem where response = 'TEMP'");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int id = rs.getInt(1);
+        System.out.println(id);
+
+        ps = connection.prepareStatement("UPDATE problem SET response = ? WHERE problem_id = " + id);
+        ps.setString(1, input.response);
+        ps.execute();
+
+
+        // set the conditions
+        for(int i = 0; i < input.conditions.size(); i++){
+            ps = connection.prepareStatement("INSERT INTO `condition` (problem_id, concept, status) VALUES(?,?,?)");
+            ps.setInt(1,id);
+            ps.setString(2,input.conditions.get(i).concept);
+            ps.setInt(3,input.conditions.get(i).status);
+            ps.execute();
+        }
+    }
 
     public static void main(String[] args) {
         try{
+            Problem prob = new Problem("response", 0);
+            prob.addCondition(new Condition("concept1", 0));
+            prob.addCondition(new Condition("concept2", 1));
             DatabaseAPI dapi = new DatabaseAPI();
-            System.out.println(dapi.getProblems());
+            dapi.addProblem(prob);
         }catch(Exception e){
             e.printStackTrace();
         }
