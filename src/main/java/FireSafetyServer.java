e7b0bbdc35ba15cfb7d9c0e732005c940cc9ead6
Kevin Vosburgh <kvosbur@purdue.edu>
2018-10-20 20:53:44 -0400

    slight changes.

--- src/main/java/FireSafetyServer.java
+++ src/main/java/FireSafetyServer.java
@@ -46,13 +46,16 @@ public class FireSafetyServer
             List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(),Charset.defaultCharset());
             Map<String, String> params = toMap(pairs);
             String[] b = params.get("images").split(",");
+            ArrayList<String> urls = new ArrayList<>();
             for(int i = 0; i < b.length; i++){
-                b[i] = URLDecoder.decode(b[i]);
+                urls.add(URLDecoder.decode(b[i]));
             }
 
+            DataHandler dh = new DataHandler();
+            DatabaseAPI dapi = new DatabaseAPI();
+            ArrayList<Problem> problems = dapi.getProblems();
 
-            return "Hope this works";
-
+            return dh.checkUserImages(problems, urls);
         });
 
         post("/concept/add", "application/json", ((request, response) ->{
@@ -61,7 +64,7 @@ public class FireSafetyServer
             JsonElement s = parser.parse(request.body());
             JsonObject dict = s.getAsJsonObject();
             String response_str = dict.get("response").getAsString();
-            JsonElement url = dict.get("urls");
+            String[] urls = dict.get("urls").getAsString().split(",");
             JsonArray conditions = dict.getAsJsonArray("conditions");
 
             ArrayList<Condition> conditionsArr = new ArrayList<>();
@@ -78,13 +81,6 @@ public class FireSafetyServer
             Problem prob = new Problem(response_str, type);
             prob.conditions = conditionsArr;
             System.out.println(prob);
-
-            //decode urls
-            JsonArray b = dict.get("urls").getAsJsonArray();
-            ArrayList<String> urls_decoded = new ArrayList<>();
-            for(int i = 0; i < b.size(); i++){
-                urls_decoded.add(URLDecoder.decode(b.get(i).getAsString()));
-            }
 
             return "";
         }));
